a9becaf511c8bbcd48c0569a0aafee9e89074b3e
rajendraprasad14 <raj.sharmakiet14@gmail.com>
2025-04-30 07:52:50 +0530

    Synchronizing job mygluejob from AWS Glue at 2025/04/30 02:22:50

--- mygluejob/mygluejob.py
+++ mygluejob/mygluejob.py
@@ -14,8 +14,13 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-df=spark.read.format("csv")option("header","True").option("path","s3://mybucket27apr/details.csv").load()
-df.write.format("csv").option("header","True").option("path","s3://mybucket27apr/output").save()
+dynamic_frame=glueContext.create_dynamic_frame.from_catalog(
+    database="etl_27db",
+    table_name="mybucket27apr"
+    )
+df=dynamic_frame.toDF()
+# df=spark.read.format("csv").option("header","True").option("path","s3://mybucket27apr/details.csv").load()
+df.write.format("csv").option("header","True").option("path","s3://mybucket27apr/output_apr_28").save()
 
 
 
